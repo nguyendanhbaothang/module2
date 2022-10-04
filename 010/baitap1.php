f4a0930e926982d2e0c56258eca201d0058191bb
nguyendanhbaothang <nguyendanhbaothang9a@gmail.com>
2022-10-04 09:38:46 +0700

    bài tập

--- 010/baitap1.php
+++ 010/baitap1.php
@@ -1,66 +1,58 @@
 <?php
-class Bracket{
-    public array $stack;
-    public function __construct(){
-        $this->stack = [];
-    }
-    public function isEmpty(): bool{
-        return empty($this->stack);
-    }
-    function hasMatchedParenthesis($string) {
-        $string = str_split($string);
-        $stack = array();
-        foreach($string as $key=>$value){
-            switch ($value) {
-                case '(': array_push($stack, 0); break;
-                case ')':
-                    if (array_pop($stack) !== 0)
-                        return false;
-                break;
-                case '[': array_push($stack, 1); break;
-                case ']':
-                    if (array_pop($stack) !== 1)
-                        return false;
-                break;
-                default: break;
-                case '{': array_push($stack, 2); break;
-                case '}':
-                    if (array_pop($stack) !== 2)
-                        return false;
-                break;
-            }
-        }
-        return (empty($stack));
-    }
-    public function isTrue($string)
-    {
-        if ($this->hasMatchedParenthesis($string)) {
-            echo "Đúng rồi nè";
-        } else {
-            echo "Sai công thức";
-        }
+class Dance{
+    public $name;
+    public $gender;
+    public function __construct($ts_name,$ts_gender){
+        $this->name = $ts_name;
+        $this->gender = $ts_gender;
     }
 }
-$checkBracket = new Bracket();
-$string1 = "s * (s – a) * (s – b) * (s – c) ";
-echo $string1."<br>";
-echo $checkBracket->isTrue($string1)."<hr>";
-$string2 =  "(– b + (b2 – 4*a*c)^0.5) / 2*a";
-echo $string2."<br>";
-echo $checkBracket->isTrue($string2)."<hr>";
-$string3 = " s * (s – a) * (s – b * (s – c)";
-echo $string3."<br>";
-echo $checkBracket->isTrue($string3)."<hr>";
-$string4 = "s * (s – a) * s – b) * (s – c) ";
-echo $string4."<br>";
-echo $checkBracket->isTrue($string4)."<hr>";
-$string5 = "(– b + (b^2 – 4*a*c)^(0.5/ 2*a))";
-echo $string5."<br>";
-echo $checkBracket->isTrue($string5)."<hr>";
+$maleQueue = new SplQueue();
+$maleQueue->enqueue (new Dance('Bảo Thắng','male'));
+$maleQueue->enqueue(new Dance(' NHân','male'));
+$maleQueue->enqueue(new Dance('Linh','male'));
+$maleQueue->enqueue(new Dance('X Thắng','male'));
+$maleQueue->enqueue(new Dance('Phong Tâm','male'));
+$maleQueue->enqueue(new Dance('Tài Tâm','male'));
+
+$femaQueue = new SplQueue();
+$femaQueue->enqueue(new Dance('Duy','female'));
+$femaQueue->enqueue(new Dance('Huyền','female'));
+$femaQueue->enqueue(new Dance('Thảo Tâm','female'));
+$femaQueue->enqueue(new Dance('P Thảo','female'));
+$femaQueue->enqueue(new Dance('Thắm','female'));
 
 
 
 
+$maleQueue->rewind();
+
+
+$femaQueue->rewind();
 
 
 
+$pairs=[];
+$female_waitting =[];
+$male_waitting =[];
+while($maleQueue->valid()||$femaQueue->valid()){
+    if($maleQueue->valid()&&$femaQueue->valid()){
+        $pairs[]=[
+            'male'=>$maleQueue->current(),
+            'female'=>$femaQueue->current(),
+        ];
+        $maleQueue->next();
+        $femaQueue->next();
+    }elseif(!$femaQueue->valid()){
+        $male_waitting[]= $maleQueue->current();
+        $maleQueue->next();
+    }elseif(!$maleQueue->valid()){
+        $female_waitting[]= $femaleQueue->current();
+        $femaleQueue->next();
+    }
+}
+echo '<pre>';
+print_r($pairs);
+print_r($female_waitting);
+print_r($male_waitting);
+die();
