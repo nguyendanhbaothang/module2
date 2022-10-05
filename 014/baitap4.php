316483af7b337d374872415e761b4e68de466fa9
nguyendanhbaothang <nguyendanhbaothang9a@gmail.com>
2022-10-05 13:59:38 +0700

    bài tập

--- 014/baitap4.php
+++ 014/baitap4.php
@@ -1,42 +1,27 @@
 <?php
-$input = '0912345678,0905111222,0978444333,0942345678';
-$numbers = explode(',',$input);
-
-// $numbers = ['0912345678','0905111222','0978444333','0942345678'];
-$vinas = [];
-$mobis = [];
-$vietels = [];
-foreach( $numbers as $number ){
-    $pattern = '/^091/';//Bắt đầu 091
-    $pattern = '/^(091|094)/';//Bắt đầu 091 Hoặc 094
-    if (preg_match($pattern, $number)) {
-        // array_push($vinas,$number);
-        $vinas[] = $number;
-    }
-
-    $pattern = '/^(090|092)/';
-    // Kiem tra -> push vao mobis
-    if (preg_match($pattern, $number)) {
-        $mobis[] = $number;
-    }
-
-    $pattern = '/^(097|096)/';
-    // Kiem tra -> push vao vietels
-    if (preg_match($pattern, $number)) {
-        $vietels[] = $number;
-    }
+if ( $_SERVER['REQUEST_METHOD']== 'POST') {
+    $nhap = $_REQUEST['nhap'];
+    $pattern = '/^\([0-9]{2,2}\)\-\([0-9]{10,10}\)$/';
+        if (preg_match($pattern, $nhap)) {
+            echo 'Số điện thoại : '.$nhap.' hợp lệ .';
+        } else {
+            echo 'Số điện thoại : '.$nhap.' không hợp lệ';
+        }      
 }
-echo "<pre>";
-echo 'số vinaphone là :';
-print_r($vinas);
-echo "</pre>";
-
-echo "<pre>";
-echo 'số mobile là :';
-print_r($mobis);
-echo "</pre>";
-
-echo "<pre>";
-echo 'số viettel là :';
-print_r($vietels);
-echo "</pre>";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="" method="post">
+  <label for="fname">Nhập số điện thoại muốn kiểm tra :</label><br>
+  <input type="text" id="" name="nhap" value=""><br>
+  <input type="submit" value="Kiểm tra">
+</form>
+</body>
+</html>
